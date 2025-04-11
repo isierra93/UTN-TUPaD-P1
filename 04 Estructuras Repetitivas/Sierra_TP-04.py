d80de393d333dbbb226fbd6ac7a194646a0f0e40
Ivan Sierra <sierraivan93@gmail.com>
2025-04-10 22:03:17 -0300

    Entrega TP 4

---
+++ 04 Estructuras Repetitivas/Sierra_TP-04.py
@@ -0,0 +1,13 @@
+"""
+Actividades
+1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+"""
+for i in range(101):
+    print(i)
+
+"""
+2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+dígitos que contiene.
+"""
+numero_02 = int(input("Ingrese un numero entero:"))
